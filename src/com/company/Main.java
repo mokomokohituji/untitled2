bbd07cba4e9636f7f2e41721685656b8c322a98d
main <mokomokohituji@users.noreply.github.com>
2021-06-24 21:43:57 +0900

    ちゃんと反映されるか確認しようとしている

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("テスト中");
+        int sum = 0;
+        for (int i = 0; i <= 10; i++) {
+            sum += 1;
+        }
+        System.out.println("うまいこといかない");
+        System.out.println(sum);
+    }
+}
